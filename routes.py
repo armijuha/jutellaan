397a138de0571410d4470253a1719a779c2e060a
Juha Arminen <juha.arminen@helsinki.fi>
2020-08-12 15:19:05 +0300

    Add templates error, layout, login and register.html

--- routes.py
+++ routes.py
@@ -1,9 +1,37 @@
 from app import app
 import users
-from flask import render_template
+from flask import render_template, redirect, request
 
 @app.route("/")
 def index():
     count = users.get_counter()
     return render_template("index.html", count=count)
 
+@app.route("/login", methods=["get","post"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+        if users.login(name,password):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Väärä tunnus tai salasana")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["get","post"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+        if users.register(name,password):
+            return redirect("/")
+        else:
+            return render_template("error.html",message="Rekisteröinti epäonnistui")
--- users.py
+++ users.py
@@ -1,6 +1,38 @@
 from db import db
+from flask import session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 def get_counter():
     result = db.session.execute("SELECT COUNT(*) FROM users")
     count = result.fetchone()[0]
     return count
+
+def login(name,password):
+    sql = "SELECT password, id FROM users WHERE name=:name"
+    result = db.session.execute(sql, {"name":name})
+    user = result.fetchone()
+    if user == None:
+        return False
+    else:
+        if check_password_hash(user[0],password):
+            session["user_id"] = user[1]
+            return True
+        else:
+            return False
+
+def logout():
+    del session["user_id"]
+
+def register(name,password):
+    hash_value = generate_password_hash(password)
+    type = 1
+    try:
+        sql = "INSERT INTO users (name,password,type) VALUES (:name,:password,:type)"
+        db.session.execute(sql, {"name":name,"password":hash_value,"type":type})
+        db.session.commit()
+    except:
+        return False
+    return login(name,password)
+
+def user_id():
+    return session.get("user_id",0)
